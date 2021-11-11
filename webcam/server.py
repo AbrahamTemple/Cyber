e558f6a08eb42b205b9121883ad2fc8a7531b7d5
Abraham Vong <73206726+AbrahamTemple@users.noreply.github.com>
2021-11-12 03:44:14 +0800

    调取摄像头发送快照

---
+++ webcam/server.py
@@ -0,0 +1,82 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import logging
+import os
+
+
+class Server(BaseHTTPRequestHandler):
+    def do_HEAD(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+        self.send_header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+        self.end_headers()
+
+    def do_GET(self):
+        paths = {
+            '/foo': {'status': 200},
+            '/bar': {'status': 302},
+            '/baz': {'status': 404},
+            '/qux': {'status': 500}
+        }
+
+        if self.path in paths:
+            self.respond(paths[self.path])
+        else:
+            self.respond({'status': 500})
+            
+        logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
+        self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
+        post_data = self.rfile.read(content_length) # <--- Gets the data itself
+
+        # logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
+        #      str(self.path), str(self.headers), post_data.decode('utf-8'))
+
+        filename = "user.txt"
+        with open(filename,"a+") as fw:
+            fw.write(post_data.decode('utf-8'))
+
+        res = "POST request data was successfully written to the file"
+
+        self.do_HEAD()
+        # self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
+        self.wfile.write("{}".format(res).encode('utf-8'))
+        # self.wfile.write("POST request for {ASS}".format(data).encode('utf-8'))
+
+    def respond(self, opts):
+        response = self.handle_http(opts['status'], self.path)
+        self.wfile.write(response)
+
+    def handle_http(self, status_code, path):
+        self.send_response(status_code)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        content = '''<p>Your accessed path: {}</p>'''.format(path)
+        return bytes(content, 'UTF-8')
+
+
+def run(server_class=HTTPServer, handler_class=Server, port=8080):
+    print("Server is start running in {} port...".format(port))
+    logging.basicConfig(level=logging.INFO)
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    logging.info('Httpd is started...\n')
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    print("Server required to close")
+    logging.info('Httpd is over...\n')
+
+
+if __name__ == '__main__':
+    from sys import argv
+
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run()
