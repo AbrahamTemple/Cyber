a0827bd2dde772631a99ec2b46ce555b029c9d72
Abraham Vong <73206726+AbrahamTemple@users.noreply.github.com>
2021-11-14 02:16:40 +0800

    更新了接口数据

--- webcam/server.py
+++ webcam/server.py
@@ -1,7 +1,7 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import logging
 import os
-
+import json
 
 class Server(BaseHTTPRequestHandler):
     def do_HEAD(self):
@@ -14,19 +14,19 @@ class Server(BaseHTTPRequestHandler):
 
     def do_GET(self):
         paths = {
-            '/foo': {'status': 200},
-            '/bar': {'status': 302},
-            '/baz': {'status': 404},
-            '/qux': {'status': 500}
+            '/ok': {'status': 200}, # return data of success
+            '/no': {'status': 203}, # return data of invalid
+            '/all': {'status': 206}, # return all of data
+            '/get': {'status': 305}, # retuen the ones of data
+            '/': {'status': 401} # verify that the server is running
         }
 
         if self.path in paths:
             self.respond(paths[self.path])
         else:
-            self.respond({'status': 500})
-            
-        logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
-        self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
+            self.respond({'status': 502})
+        #logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
+        #self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
 
     def do_POST(self):
         content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
@@ -35,26 +35,59 @@ class Server(BaseHTTPRequestHandler):
         # logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
         #      str(self.path), str(self.headers), post_data.decode('utf-8'))
 
-        filename = "user.txt"
-        with open(filename,"a+") as fw:
-            fw.write(post_data.decode('utf-8'))
-            fw.close
+        filename = "user.json"
+        with open(filename,"a+") as f:
+            f.seek(0,0) # move the cursor to the beginning of the file
+            content = f.read()
+            if content=='':
+                dict = []
+                dict.append(json.loads(post_data.decode('utf-8')))
+                json.dump(dict,f)
+            else:
+                dict = list(eval(content))
+                dict.append(json.loads(post_data.decode('utf-8')))
+                f.seek(0)
+                f.truncate()
+                json.dump(dict,f)
+                    
         res = "POST request data was successfully written to the file"
 
         self.do_HEAD()
         # self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
         self.wfile.write("{}".format(res).encode('utf-8'))
         # self.wfile.write("POST request for {ASS}".format(data).encode('utf-8'))
-
+        
     def respond(self, opts):
-        response = self.handle_http(opts['status'], self.path)
-        self.wfile.write(response)
+        filename = "user.json"
+        with open(filename,"r") as fr:
+            dict = json.loads(fr.read())
+        if opts['status'] == 200:
+            for d in dict:
+                if d['state']=='OK':
+                    response = self.handle_http(opts['status'], d['data'])
+                    self.wfile.write(response)
+        elif opts['status'] == 203:
+            for d in dict:
+                if d['state']=='NO':
+                    response = self.handle_http(opts['status'], d['data'])
+                    self.wfile.write(response)
+        elif opts['status'] == 206:
+            for d in dict:
+                response = self.handle_http(opts['status'], d['data'])
+                self.wfile.write(response)
+        elif opts['status'] == 305:
+            for d in dict:
+                response = self.handle_http(opts['status'], d['data'])
+                self.wfile.write(response)
+        else: 
+            response = self.handle_http(opts['status'], self.path)
+            self.wfile.write(response)
 
-    def handle_http(self, status_code, path):
-        self.send_response(status_code)
+    def handle_http(self, code, data):
+        self.send_response(code)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-        content = '''<p>Your accessed path: {}</p>'''.format(path)
+        content = '''{key} => {value} \n'''.format(**{"key": code, "value":data})
         return bytes(content, 'UTF-8')
 
 
